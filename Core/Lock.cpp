f3cfc623d8313dd44ceb19cb75f17181b75c561a
Alex <enderice2@users.noreply.github.com>
2022-11-01 01:53:55 +0200

    Show a number when printing a potential deadlock

--- Core/Lock.cpp
+++ Core/Lock.cpp
@@ -2,10 +2,13 @@
 
 #include <debug.h>
 
+static unsigned long DeadLocks = 0;
+
 extern "C" void DeadLockHandler(LockClass *Lock)
 {
-    warn("Potential deadlock in lock '%s' held by '%s'",
+    warn("Potential deadlock in lock '%s' held by '%s' (%ld)",
          Lock->GetLockData()->AttemptingToGet,
-         Lock->GetLockData()->CurrentHolder);
+         Lock->GetLockData()->CurrentHolder,
+         DeadLocks++);
     // TODO: Print on screen too.
 }
