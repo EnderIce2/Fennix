298c6b392193e762b8764346599be9c72717e318
Alex <enderice2@users.noreply.github.com>
2023-03-19 22:14:11 +0200

    Update page table code

--- Architecture/amd64/Bootstrap/MB2PageTable.asm
+++ Architecture/amd64/Bootstrap/MB2PageTable.asm
@@ -1,14 +1,10 @@
-%define KERNEL_OFFSET   0xFFFFFFFF80000000
-%define V2P(a) ((a) - KERNEL_OFFSET)
-
-%define PAGE_PRESENT    0x001
-%define PAGE_WRITE      0x002
-%define PAGE_GLOBAL     0x100
-
-%define PAGE_SIZE       0x1000
-%define ENTRIES_PER_PT  512
-
 section .data
-align PAGE_SIZE
+align 0x1000
 global BootPageTable
 BootPageTable:
+    dq 0x0000000000000083
+    dq 0x0000000000000083
+    TIMES (512-2) dq 0
+    dq 0x0000000000000083
+    dq 0x0000000000000083
+    TIMES (512-2) dq 0
