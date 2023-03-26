e6c103c671dc85d561a9cef39400c152f22759a4
Alex <enderice2@users.noreply.github.com>
2023-03-26 21:16:26 +0300

    Ignore frame-address warning

--- Core/Lock.cpp
+++ Core/Lock.cpp
@@ -5,7 +5,11 @@
 
 #include "../kernel.h"
 
-// #define PRINT_BACKTRACE
+#define PRINT_BACKTRACE
+
+#ifdef PRINT_BACKTRACE
+#pragma GCC diagnostic ignored "-Wframe-address"
+#endif
 
 bool ForceUnlock = false;
 Atomic<size_t> LocksCount = 0;
