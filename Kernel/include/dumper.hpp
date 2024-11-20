c4e458fcc6fcf121cec52ac4a6dfd96c11e5d1fe
EnderIce2 <enderice2@users.noreply.github.com>
2024-11-20 05:16:57 +0200

    Kernel/multiboot2_64

--- Kernel/include/dumper.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __FENNIX_LIB_DUMPER_H__
-#define __FENNIX_LIB_DUMPER_H__
-
-#include <types.h>
-
-void DumpData(const char *Description, void *Address, unsigned long Length);
-
-#endif // !__FENNIX_LIB_DUMPER_H__
--- Kernel/include/stdint.h
+++
@@ -1,6 +0,0 @@
-#ifndef __FENNIX_KERNEL_STUB_STDINT_H__
-#define __FENNIX_KERNEL_STUB_STDINT_H__
-
-#include <types.h>
-
-#endif // !__FENNIX_KERNEL_STUB_STDINT_H__
