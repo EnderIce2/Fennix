4ced264c3cfd94a3d0a91eaf477b66000e7d765e
EnderIce2 <enderice2@protonmail.com>
2025-01-24 23:57:15 +0200

    userspace: simplify SIMD test application assembly code
    Signed-off-by: EnderIce2 <enderice2@protonmail.com>

--- Userspace/apps/test/simd_test/simd.c
+++ Userspace/apps/test/simd_test/simd.c
@@ -1,9 +1,7 @@
-int main(int, char *argv[], char *[])
+int main(int, char *[], char *[])
 {
 #if defined(__amd64__) || defined(__i386__)
-	__asm__ __volatile__("movaps (%0), %%xmm0\n"
-						 "movaps %%xmm0, (%0)\n"
-						 : : "r"(argv) : "memory");
+	__asm__ __volatile__("movaps %%xmm1, %%xmm0\n" : : : "memory");
 #else
 #warning "Unimplemented"
 #endif
