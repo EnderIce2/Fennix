bd1d1172830155fa71220cea7a290a3e1deaad7f
EnderIce2 <enderice2@protonmail.com>
2025-02-12 02:51:14 +0200

    userspace/libc: add ioctl function
    Signed-off-by: EnderIce2 <enderice2@protonmail.com>

---
+++ Userspace/libc/include/sys/ioctl.h
@@ -0,0 +1,25 @@
+/*
+	This file is part of Fennix C Library.
+
+	Fennix C Library is free software: you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of
+	the License, or (at your option) any later version.
+
+	Fennix C Library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Fennix C Library. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _SYS_IOCTL_H
+#define _SYS_IOCTL_H
+
+#include <sys/types.h>
+
+int ioctl(int fd, unsigned long op, ...);
+
+#endif // !_SYS_IOCTL_H
