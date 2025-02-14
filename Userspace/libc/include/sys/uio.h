d0a8d9dd620a7b0010e933fffb2577217f4bd3de
EnderIce2 <enderice2@protonmail.com>
2025-02-14 02:15:40 +0200

    userspace/libc: add <sys/uio.h> header for vector I/O operations
    Signed-off-by: EnderIce2 <enderice2@protonmail.com>

---
+++ Userspace/libc/include/sys/uio.h
@@ -0,0 +1,32 @@
+/*
+	This file is part of Fennix C Library.
+
+	Fennix C Library is free software: you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of
+	the License, or (at your option) any later version.
+
+	Fennix C Library is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Fennix C Library. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifndef _SYS_UIO_H_
+#define _SYS_UIO_H_
+
+#include <sys/types.h>
+
+struct iovec
+{
+	void *iov_base; /* Base address of a memory region for input or output */
+	size_t iov_len; /* The size of the memory pointed to by iov_base */
+};
+
+ssize_t readv(int fildes, const struct iovec *iov, int iovcnt);
+ssize_t writev(int fildes, const struct iovec *iov, int iovcnt);
+
+#endif // _SYS_UIO_H_
