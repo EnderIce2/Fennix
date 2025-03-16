babf792c3056055f1dd54f20e60a05833448d3b8
EnderIce2 <enderice2@protonmail.com>
2025-03-16 00:47:56 +0000

    feat(userspace/coreutils): add stub "admin" command
    https://pubs.opengroup.org/onlinepubs/9799919799/utilities/admin.html
    Signed-off-by: EnderIce2 <enderice2@protonmail.com>

---
+++ Userspace/coreutils/src/admin.c
@@ -0,0 +1,32 @@
+/*
+	This file is part of Fennix Core Utilities.
+
+	Fennix Core Utilities is free software: you can redistribute it and/or
+	modify it under the terms of the GNU General Public License as
+	published by the Free Software Foundation, either version 3 of
+	the License, or (at your option) any later version.
+
+	Fennix Core Utilities is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Fennix Core Utilities. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+	fprintf(stderr, "stub\n");
+	return EXIT_FAILURE;
+}
