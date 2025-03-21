ae7f39d0deb990b23a4283188403ab855b3feb48
EnderIce2 <enderice2@protonmail.com>
2025-03-21 03:22:40 +0000

    feat(kernel/drivers): add trusted drivers list
    Signed-off-by: EnderIce2 <enderice2@protonmail.com>

---
+++ Kernel/drivers/trusted.c
@@ -0,0 +1,4 @@
+const char *trusted_drivers[] = {
+"ae08d2e120c8370278ca9e17085a6b9e2f4a470ab6cec824c77ab1f8706c7144f5d4e1c9820914ed4fc7a4fd22de4b18bfed7c3b5d9c1e604e82280d7d45a5c7",
+};
+const __SIZE_TYPE__ trusted_drivers_count = sizeof(trusted_drivers) / sizeof(trusted_drivers[0]);
